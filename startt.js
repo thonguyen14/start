3cfbd0cc4e073e6b449044b8552fd70725c67d3a
thonguyen14 <80441573+thonguyen14@users.noreply.github.com>
2024-03-02 23:14:26 +0700

    Update and rename start.js to startt.js

--- startt.js
+++ startt.js
@@ -3,11 +3,11 @@
 const os = require('os'),
    _exec = require('child_process').exec;
 
-function start(process) {
+function startt(process) {
     if(os.platform() === 'win32')
         _exec(`start ${process}`);
     else
         _exec(`xterm -e ${process}`);
 }
 
-module.exports = start;
+module.exports = startt;
